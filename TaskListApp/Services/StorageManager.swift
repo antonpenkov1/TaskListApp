06e9b51822a56e22a420ba45802ea5333b609655
anton_penkov <anton.penkov1@gmail.com>
2024-02-12 21:14:15 +0100

    StorageManager added

--- TaskListApp/Services/StorageManager.swift
+++ TaskListApp/Services/StorageManager.swift
@@ -1,30 +1,16 @@
 //
-//  AppDelegate.swift
+//  StorageManager.swift
 //  TaskListApp
 //
-//  Created by Alexey Efimov on 11.02.2024.
+//  Created by Антон Пеньков on 12.02.2024.
 //
 
-import UIKit
+import Foundation
 import CoreData
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-
+final class StorageManager {
+    static let shared = StorageManager()
+    
     // MARK: - Core Data stack
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "TaskListApp")
@@ -36,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return container
     }()
 
+    private init() {}
+    
     // MARK: - Core Data Saving support
     func saveContext() {
         let context = persistentContainer.viewContext
@@ -48,6 +36,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
 }
-
