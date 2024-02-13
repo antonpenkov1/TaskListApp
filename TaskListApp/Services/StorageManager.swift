93b11a358d016e28914d59d48f1dc3a405f70b46
anton_penkov <anton.penkov1@gmail.com>
2024-02-13 01:21:50 +0100

    Working, but looking bad, plus deselectRow doesn't want to work...

--- TaskListApp/Services/StorageManager.swift
+++ TaskListApp/Services/StorageManager.swift
@@ -36,5 +36,5 @@ final class StorageManager {
             }
         }
     }
-    
+
 }
--- TaskListApp/ViewControllers/TaskListViewController.swift
+++ TaskListApp/ViewControllers/TaskListViewController.swift
@@ -21,7 +21,7 @@ final class TaskListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        showAlert(withTitle: "New Task", andMessage: "What do you want to do?")
+        showAlert(withTitle: "New Task", message: "What do you want to do?", andText: nil, at: 0)
     }
     
     private func fetchData() {
@@ -34,17 +34,22 @@ final class TaskListViewController: UITableViewController {
         }
     }
     
-    private func showAlert(withTitle title: String, andMessage message: String) {
+    private func showAlert(withTitle title: String, message: String, andText text: String?, at index: Int) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save Task", style: .default) { [unowned self] _ in
             guard let taskName = alert.textFields?.first?.text, !taskName.isEmpty else { return }
-            save(taskName)
+            if text == nil {
+                save(taskName)
+            } else {
+                update(toDoTask: taskList[index], with: taskName)
+            }
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
         alert.addTextField { textField in
             textField.placeholder = "New Task"
+            textField.text = text
         }
         present(alert, animated: true)
     }
@@ -57,6 +62,18 @@ final class TaskListViewController: UITableViewController {
         let indexPath = IndexPath(row: taskList.count - 1, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
         
+        storageManager.saveContext()
+    }
+    
+    private func delete(at index: Int) {
+        storageManager.persistentContainer.viewContext.delete(taskList[index])
+        taskList.remove(at: index)
+        storageManager.saveContext()
+    }
+    
+    private func update(toDoTask: ToDoTask, with title: String) {
+        toDoTask.title = title
+        tableView.reloadData()
         storageManager.saveContext()
     }
 }
@@ -75,6 +92,28 @@ extension TaskListViewController {
         cell.contentConfiguration = content
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        showAlert(
+            withTitle: "Edit Task",
+            message: "What do you want to do?",
+            andText: taskList[indexPath.row].title ?? "", 
+            at: indexPath.row
+        )
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            delete(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
 }
 
 // MARK: - Setup UI
